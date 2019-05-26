481f53606dad26efd6dc4692da50521e4b17feb7
Amos Wong <amoswwj@gmail.com>
2019-05-26 22:25:27 +0800

    refactor: Extract parseCalledId into a function

--- src/router.js
+++ src/router.js
@@ -1,3 +1,16 @@
+export const ROUTE_TYPE = {
+  Enter: 'enter',
+  Exit: 'exit',
+};
+
+export const parseCallerId = (callerId) => {
+  const i = callerId.indexOf(':');
+  return {
+    type: callerId.slice(0, i).split('=')[1] || ROUTE_TYPE.Enter,
+    route: callerId.slice(i + 1),
+  };
+}
+
 export default class Router {
   constructor(routes, context) {
     this.context = context;
@@ -14,12 +27,8 @@ export default class Router {
     return Promise.resolve()
       .then(() => {
         // Make route
-        const callerInfo = locals.callerdebug || locals.caller2 || locals.caller1 || '';
-        const i = callerInfo.indexOf(':');
-        const caller = {
-          type: callerInfo.slice(0, i).split('=')[1] || 'enter',
-          route: callerInfo.slice(i + 1),
-        };
+        const callerId = locals.callerdebug || locals.caller2 || locals.caller1 || '';
+        const caller = parseCallerId(callerId);
 
         // Go to route
         const route = this.routes[caller.route] || this.routes.ui;
